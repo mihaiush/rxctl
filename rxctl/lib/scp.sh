628b1b81883f586750f11669c7c2b541b271056a
Mihai Craiu <mihai.craiu@1and1.ro>
2022-12-12 12:46:01 +0200

    examples

--- rxctl/lib/scp.sh
+++ rxctl/lib/scp.sh
@@ -1,4 +1,8 @@
-#!/bin/sh -e
+#!/bin/bash -e
+
+if [ $RX_LOG_VERBOSITY -ge 2 ] ; then
+    set -x
+fi
 
 if [ "${1}" = "-r" ] ; then
     R="-r "
@@ -9,14 +13,15 @@ fi
 
 CMD=$(basename $0)
 
-if [ "$CMD" == "__put" ] ; then
+if [ "$CMD" = "__put" ] ; then
     SRC="${1}"
     DST=${RX_HOST}:"${2}"
-elif [ "$CMD" == "__get" ] ; then
+elif [ "$CMD" = "__get" ] ; then
     SRC=${RX_HOST}:"${1}"
     DST="${2}"
 else
     exit 1
 fi
+
 __log.debug "$CMD: '${SRC}' -> '${DST}'"
 ${RX_SCP_CMD} ${R}"${SRC}" "${DST}"
